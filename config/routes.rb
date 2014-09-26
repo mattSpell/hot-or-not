fe87f7730d51b357888ccfc8800f83c177899699
Matt Spell <matthew.spell1@gmail.com>
2014-09-25 20:11:17 -0500

    Mobile Styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  # get '/favorites' => 'favorites#index'
   resources :favorites, only: [:index, :destroy]
 
   root "home#index"
